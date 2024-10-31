823d546e475e19c1ae5f27b2a2701e86ed017645
Will Hegedus <whegedus@akamai.com>
2024-10-31 14:00:00 -0400

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/custom/plugins/init.lua Add .config/nvim/lua/kickstart/health.lua Add .config/nvim/lua/kickstart/plugins/autopairs.lua Add .config/nvim/lua/kickstart/plugins/debug.lua Add .config/nvim/lua/kickstart/plugins/gitsigns.lua Add .config/nvim/lua/kickstart/plugins/indent_line.lua Add .config/nvim/lua/kickstart/plugins/lint.lua Add .config/nvim/lua/kickstart/plugins/neo-tree.lua

---
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,38 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    -- `BufReadPre` event to prevent show a `No Name` buffer when open a directory use nvim directly
+    event = 'BufReadPre',
+    opts = {
+      options = {
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Neo-tree',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
+      },
+    },
+    keys = {
+      { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
+      { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
+      { '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', desc = 'Delete Other Buffers' },
+      { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
+      { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
+      { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+      { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+      { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+      { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+      { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
+      { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+    },
+  },
+}
