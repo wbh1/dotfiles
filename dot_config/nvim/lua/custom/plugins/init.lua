49d34c853cbc1560f7b27889c6e8d6a7f5910a26
Will Hegedus <will@wbhegedus.me>
2024-11-17 14:54:46 -0500

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lua/custom/plugins/init.lua

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -3,81 +3,107 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'akinsho/bufferline.nvim',
-    version = '*',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    -- `BufReadPre` event to prevent show a `No Name` buffer when open a directory use nvim directly
-    event = 'BufReadPre',
-    opts = {
-      options = {
-        offsets = {
-          {
-            filetype = 'neo-tree',
-            text = 'Neo-tree',
-            highlight = 'Directory',
-            text_align = 'left',
-          },
-        },
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		-- `BufReadPre` event to prevent show a `No Name` buffer when open a directory use nvim directly
+		event = "BufReadPre",
+		opts = {
+			options = {
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+			},
+		},
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+			{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+			{
+				"<leader>bd",
+				"<Cmd>lua MiniBufremove.delete()<CR>",
+				desc = "Close current buffer (using mini.bufremove)",
+			},
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+			{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+		},
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		config = function()
+			require("toggleterm").setup({
+				open_mapping = [[<c-\>]],
+				shade_terminals = false,
+				-- add --login so ~/.zprofile is loaded
+				-- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
+				shell = "zsh --login",
+				direction = "float",
+				float_opts = {
+					border = "curved",
+				},
+			})
+		end,
+		keys = {
+			{ [[<C-\>]] },
+			{ "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+			{
+				"<leader>td",
+			},
+		},
+	},
+	{
+		"ellisonleao/glow.nvim",
+		config = function()
+			require("glow").setup({
+				border = "double",
+				width = 120,
+			})
+		end,
+		cmd = "Glow",
+	},
+	{
+		"stevearc/aerial.nvim",
+		opts = {
+			attach_mode = "global",
+			backends = { "lsp", "treesitter", "markdown", "man" },
+			show_guides = true,
+			layout = {
+				resize_to_content = false,
+				win_opts = {
+					winhl = "Normal:NormalFloat,FloatBorder:NormalFloat,SignColumn:SignColumnSB",
+					signcolumn = "yes",
+					statuscolumn = " ",
+				},
+			},
+      -- stylua: ignore
+      guides = {
+        mid_item   = "├╴",
+        last_item  = "└╴",
+        nested_top = "│ ",
+        whitespace = "  ",
       },
-    },
-    keys = {
-      { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
-      { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
-      { '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', desc = 'Delete Other Buffers' },
-      { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
-      { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
-      { '<leader>bd', '<Cmd>lua MiniBufremove.delete()<CR>', desc = 'Close current buffer (using mini.bufremove)' },
-      { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
-      { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
-      { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
-      { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
-      { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
-      { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
-    },
-  },
-  {
-    'akinsho/toggleterm.nvim',
-    config = function()
-      require('toggleterm').setup {
-        open_mapping = [[<c-\>]],
-        shade_terminals = false,
-        -- add --login so ~/.zprofile is loaded
-        -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
-        shell = 'zsh --login',
-        direction = 'float',
-        float_opts = {
-          border = 'curved',
-        },
-      }
-    end,
-    keys = {
-      { [[<C-\>]] },
-      { '<leader>0', '<Cmd>2ToggleTerm<Cr>', desc = 'Terminal #2' },
-      {
-        '<leader>td',
-      },
-    },
-  },
-  {
-    'ellisonleao/glow.nvim',
-    config = function()
-      require('glow').setup {
-        border = 'double',
-        width = 120,
-      }
-    end,
-    cmd = 'Glow',
-  },
-  {
-    'stevearc/aerial.nvim',
-    opts = {},
-    -- Optional dependencies
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons',
-    },
-  },
+		},
+		keys = {
+			{ "<leader>cs", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" },
+		},
+		-- Optional dependencies
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
 
 -- vim: ts=2 sts=2 sw=2 et
