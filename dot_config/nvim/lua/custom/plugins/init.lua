dc003f5db13be6d96b53023ac026a9f78cd97f38
Will Hegedus <whegedus@akamai.com>
2024-11-22 12:25:21 -0500

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lua/custom/plugins/init.lua Update .config/nvim/lua/kickstart/plugins/neo-tree.lua

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -108,6 +108,19 @@ return {
       'nvim-tree/nvim-web-devicons',
     },
   },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
--- dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -13,17 +13,104 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    close_if_last_window = true,
-    filesystem = {
-      filtered_items = {
-        visible = true,
-      },
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+  opts = function()
+    local renderer = require 'neo-tree.ui.renderer'
+    local indexOf = function(array, value)
+      for i, v in ipairs(array) do
+        if v == value then
+          return i
+        end
+      end
+      return nil
+    end
+    local getSiblings = function(state, node)
+      local parent = state.tree:get_node(node:get_parent_id())
+      local siblings = parent:get_child_ids()
+      return siblings
+    end
+    local next_sibling = function(state)
+      local node = state.tree:get_node()
+      local siblings = getSiblings(state, node)
+      if not node.is_last_child then
+        local currentIndex = indexOf(siblings, node.id)
+        local nextIndex = siblings[currentIndex + 1]
+        renderer.focus_node(state, nextIndex)
+      end
+    end
+    local prev_sibling = function(state)
+      local node = state.tree:get_node()
+      local siblings = getSiblings(state, node)
+      local currentIndex = indexOf(siblings, node.id)
+      if currentIndex > 1 then
+        local nextIndex = siblings[currentIndex - 1]
+        renderer.focus_node(state, nextIndex)
+      end
+    end
+    local last_sibling = function(state)
+      local tree = state.tree
+      local node = tree:get_node()
+      local siblings = tree:get_nodes(node:get_parent_id())
+      renderer.focus_node(state, siblings[#siblings]:get_id())
+    end
+    local first_sibling = function(state)
+      local tree = state.tree
+      local node = tree:get_node()
+      local siblings = tree:get_nodes(node:get_parent_id())
+      renderer.focus_node(state, siblings[1]:get_id())
+    end
+    local focus_parent = function(state)
+      local tree = state.tree
+      local node = tree:get_node()
+      renderer.focus_node(state, node:get_parent_id())
+    end
+    return {
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+            ['J'] = { next_sibling, desc = 'next_sibling' },
+            ['K'] = { prev_sibling, desc = 'prev_sibling' },
+            ['<C-j>'] = { last_sibling, desc = 'last_sibling' },
+            ['<C-k>'] = { first_sibling, desc = 'first_sibling' },
+            ['<C-p>'] = { focus_parent, desc = 'focus_parent' },
+          },
         },
       },
-    },
-  },
+    }
+  end,
 }
+-- opts = {
+--   close_if_last_window = true,
+--   filesystem = {
+--     filtered_items = {
+--       visible = true,
+--     },
+--     window = {
+--       mappings = {
+--         ['\\'] = 'close_window',
+--         ['J'] = { next_sibling, desc = 'next_sibling' },
+--         ['K'] = { prev_sibling, desc = 'prev_sibling' },
+--         -- Go to last sibling
+--         ['<C-j>'] = function(state)
+--           local tree = state.tree
+--           local node = tree:get_node()
+--           local siblings = tree:get_nodes(node:get_parent_id())
+--           local renderer = require 'neo-tree.ui.renderer'
+--           renderer.focus_node(state, siblings[#siblings]:get_id())
+--         end,
+--         -- Go to first sibling
+--         ['<C-K>'] = function(state)
+--           local tree = state.tree
+--           local node = tree:get_node()
+--           local siblings = tree:get_nodes(node:get_parent_id())
+--           local renderer = require 'neo-tree.ui.renderer'
+--           renderer.focus_node(state, siblings[1]:get_id())
+--         end,
+--       },
+--     },
+--   },
+-- },
