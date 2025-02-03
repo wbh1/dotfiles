8fa5d4ad4842fc8fdc0bf6540444876519f118df
Will Hegedus <whegedus@akamai.com>
2025-02-03 11:29:09 -0500

    Update .config/sketchybar/items/front_app.sh

--- dot_config/sketchybar/items/executable_front_app.sh
+++ dot_config/sketchybar/items/executable_front_app.sh
@@ -2,15 +2,14 @@
 # INFO="${INFO-$(aerospace list-windows --focused --format '%{app-name}')}"
 FRONT_APP_SCRIPT='sketchybar --set $NAME label="${INFO-$(aerospace list-windows --focused --format "%{app-name}")}"'
 
-sketchybar --add       event        window_focus                  \
-           --add       event        windows_on_spaces             \
-           --add       item         front_app left                \
-           --set       front_app    script="$FRONT_APP_SCRIPT"    \
-                                    icon.drawing=off              \
-                                    background.padding_left=0     \
-                                    background.padding_right=10   \
-                                    label.color=$ORANGE           \
-                                    label.font="$FONT:Black:12.0" \
-                                    associated_display=active     \
-           --subscribe front_app    front_app_switched
-
+sketchybar --add event window_focus \
+    --add event windows_on_spaces \
+    --add item front_app left \
+    --set front_app script="$FRONT_APP_SCRIPT" \
+    icon.drawing=off \
+    background.padding_left=10 \
+    background.padding_right=10 \
+    label.color=$ORANGE \
+    label.font="$FONT:Black:12.0" \
+    associated_display=active \
+    --subscribe front_app front_app_switched
