5494f3e35b3d58a9431f87a116389dec2a5be464
Will Hegedus <whegedus@akamai.com>
2025-04-24 09:41:39 -0400

    Update .config/sketchybar/sketchybarrc Update .config/sketchybar/items/calendar.sh

--- dot_config/sketchybar/items/executable_calendar.sh
+++ dot_config/sketchybar/items/executable_calendar.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-sketchybar --add item     calendar cente                    \
-           --set calendar icon=cal                          \
-                          icon.font="$FONT:Black:12.0"      \
-                          icon.padding_right=0              \
-                          label.width=50                    \
-                          label.align=right                 \
-                          background.padding_left=15        \
-                          update_freq=30                    \
-                          script="$PLUGIN_DIR/calendar.sh"  \
-                          click_script="$PLUGIN_DIR/zen.sh"
+sketchybar --add item calendar q \
+    --set calendar icon=cal \
+    icon.font="$FONT:Black:12.0" \
+    icon.padding_right=0 \
+    label.width=50 \
+    label.align=right \
+    background.padding_left=15 \
+    update_freq=30 \
+    script="$PLUGIN_DIR/calendar.sh" \
+    click_script="$PLUGIN_DIR/zen.sh"
