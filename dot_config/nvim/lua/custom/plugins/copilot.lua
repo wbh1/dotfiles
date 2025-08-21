06cb6d976a3054e15135f1ca0a19eee4e95b2332
Will Hegedus <whegedus@akamai.com>
2025-08-21 09:14:05 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/custom/plugins/copilot.lua

--- dot_config/nvim/lua/custom/plugins/copilot.lua
+++ dot_config/nvim/lua/custom/plugins/copilot.lua
@@ -1,18 +1,12 @@
--- if vim.fn.hostname() == 'oth-mpro1' then
---   return {
---     'CopilotC-Nvim/CopilotChat.nvim',
---     branch = 'canary',
---     dependencies = {
---       { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
---       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
---     },
---     build = 'make tiktoken', -- Only on MacOS or Linux
---     opts = {
---       -- See Configuration section for options
---     },
---     -- See Commands section for default commands if you want to lazy load on them
---   }
--- else
---   return {}
--- end
-return {}
+return {
+  'CopilotC-Nvim/CopilotChat.nvim',
+  dependencies = {
+    { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+    { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+  },
+  build = 'make tiktoken', -- Only on MacOS or Linux
+  opts = {
+    -- See Configuration section for options
+  },
+  -- See Commands section for default commands if you want to lazy load on them
+}
