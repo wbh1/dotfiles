95ca3270139cf514d06f23fc35a8e5f98bc1fe3c
Will Hegedus <whegedus@akamai.com>
2025-09-08 10:53:14 -0400

    Update .config/nvim/lua/kickstart/plugins/autopairs.lua

--- dot_config/nvim/lua/kickstart/plugins/autopairs.lua
+++ dot_config/nvim/lua/kickstart/plugins/autopairs.lua
@@ -1,5 +1,6 @@
 -- autopairs
 -- https://github.com/windwp/nvim-autopairs
+-- TODO: Use mini.pairs ?
 
 return {
   'windwp/nvim-autopairs',
@@ -12,5 +13,14 @@ return {
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    local np = require 'nvim-autopairs'
+    local Rule = require 'nvim-autopairs.rule'
+    local cond = require 'nvim-autopairs.conds'
+
+    -- https://github.com/windwp/nvim-autopairs/issues/478#issuecomment-2543315927
+    np.add_rules {
+      Rule('*', '*', { 'markdown' }):with_pair(cond.not_before_regex '\n'),
+      Rule('_', '_', { 'markdown' }):with_pair(cond.before_regex '%s'),
+    }
   end,
 }
