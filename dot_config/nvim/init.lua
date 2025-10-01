27fd8d91fc917f9d6bf6d3f0f9786725d07ecf75
Will Hegedus <whegedus@akamai.com>
2025-10-01 08:45:55 -0400

    Update .config/nvim/init.lua Update .config/nvim/spell/en.utf-8.add Update .config/nvim/spell/en.utf-8.add.spl

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -214,6 +214,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Enable spell checking on markdown files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_us'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
