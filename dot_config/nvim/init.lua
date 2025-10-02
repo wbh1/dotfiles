ac539e3d80b640ab63a125e8d25f8646a8e35615
Will Hegedus <whegedus@akamai.com>
2025-10-02 15:06:55 -0400

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/spell/en.utf-8.add Update .config/nvim/spell/en.utf-8.add.spl

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -522,7 +522,9 @@ require('lazy').setup({
         marksman = {
           autostart = true,
         },
-        copilot = {},
+        copilot = {
+          autostart = false,
+        },
 
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
