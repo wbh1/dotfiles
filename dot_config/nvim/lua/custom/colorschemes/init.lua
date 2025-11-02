e44bb1221fd3055b2070b9c07bfd165172f51a2d
Will Hegedus <will@wbhegedus.me>
2025-11-02 15:43:00 -0500

    Update .config/nvim/init.lua Update .config/nvim/lua/custom/colorschemes/init.lua Update .config/nvim/lua/kickstart/plugins/debug.lua

--- dot_config/nvim/lua/custom/colorschemes/init.lua
+++ dot_config/nvim/lua/custom/colorschemes/init.lua
@@ -1,3 +1,15 @@
-local theme = require 'custom.colorschemes.nightfox'
--- local theme = require 'custom.colorschemes.nord';
+-- Check if omarchy theme exists
+local omarchy_theme_path = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
+local theme
+
+-- if vim.fn.filereadable(omarchy_theme_path) == 1 then
+--   -- Load the omarchy theme if it exists
+--   theme = dofile(omarchy_theme_path)
+-- else
+--   -- Fall back to default theme
+--   theme = require 'custom.colorschemes.nightfox'
+--   -- local theme = require 'custom.colorschemes.nord';
+-- end
+
+theme = require("custom.colorschemes.nightfox")
 return { theme }
--- dot_config/nvim/lua/kickstart/plugins/debug.lua
+++ dot_config/nvim/lua/kickstart/plugins/debug.lua
@@ -6,52 +6,170 @@
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
+-- This is stolen from LazyVim
+---@param config {type?:string, args?:string[]|fun():string[]?}
+local function get_args(config)
+  local args = type(config.args) == "function" and (config.args() or {}) or config.args or {} --[[@as string[] | string ]]
+  local args_str = type(args) == "table" and table.concat(args, " ") or args --[[@as string]]
+
+  config = vim.deepcopy(config)
+  ---@cast args string[]
+  config.args = function()
+    local new_args = vim.fn.expand(vim.fn.input("Run with args: ", args_str)) --[[@as string]]
+    if config.type and config.type == "java" then
+      ---@diagnostic disable-next-line: return-type-mismatch
+      return new_args
+    end
+    return require("dap.utils").splitstr(new_args)
+  end
+  return config
+end
+
 return {
   -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
+  "mfussenegger/nvim-dap",
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+    "rcarriga/nvim-dap-ui",
 
     -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
+    "nvim-neotest/nvim-nio",
 
     -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+    "williamboman/mason.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-    'mfussenegger/nvim-dap-python',
+    "leoluz/nvim-dap-go",
+    "mfussenegger/nvim-dap-python",
   },
-  keys = function(_, keys)
-    local dap = require 'dap'
-    local dapui = require 'dapui'
-    return {
-      -- Basic debugging keymaps, feel free to change to your liking!
-      { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
-      { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
-      { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
-      { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
-      { '<leader>db', dap.toggle_breakpoint, desc = '[D]ebug: Toggle [B]reakpoint' },
-      {
-        '<leader>dB',
-        function()
-          dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-        end,
-        desc = '[D]ebug: Set conditional [B]reakpoint',
-      },
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-      { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
-      unpack(keys),
-    }
-  end,
+  keys = {
+    {
+      "<leader>dB",
+      function()
+        require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end,
+      desc = "Breakpoint Condition",
+    },
+    {
+      "<leader>db",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Toggle Breakpoint",
+    },
+    {
+      "<leader>dc",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Run/Continue",
+    },
+    {
+      "<leader>da",
+      function()
+        require("dap").continue({ before = get_args })
+      end,
+      desc = "Run with Args",
+    },
+    {
+      "<leader>dC",
+      function()
+        require("dap").run_to_cursor()
+      end,
+      desc = "Run to Cursor",
+    },
+    {
+      "<leader>dg",
+      function()
+        require("dap").goto_()
+      end,
+      desc = "Go to Line (No Execute)",
+    },
+    {
+      "<leader>di",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Step Into",
+    },
+    {
+      "<leader>dj",
+      function()
+        require("dap").down()
+      end,
+      desc = "Down",
+    },
+    {
+      "<leader>dk",
+      function()
+        require("dap").up()
+      end,
+      desc = "Up",
+    },
+    {
+      "<leader>dl",
+      function()
+        require("dap").run_last()
+      end,
+      desc = "Run Last",
+    },
+    {
+      "<leader>do",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Step Out",
+    },
+    {
+      "<leader>dO",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Step Over",
+    },
+    {
+      "<leader>dP",
+      function()
+        require("dap").pause()
+      end,
+      desc = "Pause",
+    },
+    {
+      "<leader>dr",
+      function()
+        require("dap").repl.toggle()
+      end,
+      desc = "Toggle REPL",
+    },
+    {
+      "<leader>ds",
+      function()
+        require("dap").session()
+      end,
+      desc = "Session",
+    },
+    {
+      "<leader>dt",
+      function()
+        require("dap").terminate()
+      end,
+      desc = "Terminate",
+    },
+    {
+      "<leader>dw",
+      function()
+        require("dap.ui.widgets").hover()
+      end,
+      desc = "Widgets",
+    },
+  },
   config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+    local dap = require("dap")
+    local dapui = require("dapui")
 
-    require('mason-nvim-dap').setup {
+    require("mason-nvim-dap").setup({
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_installation = true,
@@ -64,32 +182,32 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
-        'python',
+        "delve",
+        "python",
       },
-    }
+    })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
+    dapui.setup({
       -- Set icons to characters that are more likely to work in every terminal.
       --    Feel free to remove or use ones that you like more! :)
       --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
       controls = {
         icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          pause = "⏸",
+          play = "▶",
+          step_into = "⏎",
+          step_over = "⏭",
+          step_out = "⏮",
+          step_back = "b",
+          run_last = "▶▶",
+          terminate = "⏹",
+          disconnect = "⏏",
         },
       },
-    }
+    })
 
     -- Change breakpoint icons
     -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
@@ -103,18 +221,18 @@ return {
     --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
     -- end
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+    dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+    dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup {
+    require("dap-go").setup({
       delve = {
         -- On Windows delve must be run attached or it crashes.
         -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
+        detached = vim.fn.has("win32") == 0,
       },
-    }
-    require('dap-python').setup 'uv'
+    })
+    require("dap-python").setup("uv")
   end,
 }
