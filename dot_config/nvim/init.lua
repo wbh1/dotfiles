574f227a0dc2589ce3d28282397cf9c77bd038f3
Will Hegedus <whegedus@akamai.com>
2026-03-17 15:17:48 -0400

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/snippets/personal.code-snippets

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -689,7 +689,7 @@ require('lazy').setup({
         end)(),
       },
     },
-    version = 'v0.*',
+    version = '1.*',
     opts = {
       keymap = {
         preset = 'default',
@@ -768,20 +768,7 @@ require('lazy').setup({
         enabled = true,
       },
 
-      snippets = {
-        expand = function(snippet)
-          require('luasnip').lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require('luasnip').jumpable(filter.direction)
-          end
-          return require('luasnip').in_snippet()
-        end,
-        jump = function(direction)
-          require('luasnip').jump(direction)
-        end,
-      },
+      snippets = { preset = 'luasnip' },
     },
     opts_extend = { 'sources.default' },
     config = function(_, opts)
